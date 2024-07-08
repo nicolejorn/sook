cff4dcf9e0b82569d3bb65d5148630d53a2f7172
nicolejorn <sadiekesten@gmail.com>
2024-07-08 14:47:46 -0400

    Added a read me

---
+++ README.md
@@ -0,0 +1,3 @@
+"Cozy Game" inspired by Truman Capote's short story "A Christmas Memory" made with Butano https://github.com/GValiente/butano 
+Code structure based on Feline by Jono Shields https://github.com/foopod/gbaGamejam2021 and the Butano examples
+Art made by me in Usenti https://www.coranac.com/projects/usenti/ 
--- src/so_scene_shop.cpp
+++ src/so_scene_shop.cpp
@@ -15,11 +15,18 @@
 #include "bn_affine_bg_map_cell.h"
 #include "bn_display.h"
 #include "bn_sprite_font.h"
+#include "bn_rect_window.h"
+#include "bn_camera_actions.h"
 
 #include "so_player.h"
 #include "so_scene_type.h"
 #include "so_npc.h"
 #include "so_npc_type.h"
+/* #include "bn_affine_bg_big_map_canvas_size.h"
+#include "bn_affine_bg_pa_register_hbe_ptr.h"
+#include "bn_affine_bg_pd_register_hbe_ptr.h"
+#include "bn_affine_bg_dx_register_hbe_ptr.h"
+#include "bn_affine_bg_dy_register_hbe_ptr.h" */
 
 namespace so
 {
@@ -27,5 +34,11 @@ namespace so
         : _player(&player) {}
 
     Scene Shop::execute() //bn::fixed_point spawn_location
-    {}
+    {
+        //Scrolling map based on cameras from examples
+        bn::regular_bg_ptr haha_house_bg = bn::regular_bg_items::haha_house.create_bg(0, 0);
+
+        bn::camera_ptr camera = bn::camera_ptr::create(0, 0);
+        haha_house_bg.set_camera(camera);
+    }
 }
